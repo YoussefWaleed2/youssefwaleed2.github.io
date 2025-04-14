124f3f75b95fe5bdcd391040f622cfa63d4856ef
YoussefWaleed2 <114354330+YoussefWaleed2@users.noreply.github.com>
2025-04-14 19:28:37 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "./",
   build: {
     outDir: "build",
     assetsDir: "assets",
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const extType = info[info.length - 1];
-          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
-            return `assets/images/[name][extname]`;
-          }
-          if (/\.css$/i.test(assetInfo.name)) {
-            return `assets/css/[name][extname]`;
-          }
-          return `assets/[name][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
       },
     },
   },

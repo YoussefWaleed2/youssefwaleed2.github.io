da089b5d075f5df5aa60934d674e3f246517cae2
Abdelrhman Shokry <150286873+abdelrhmanshokry1212@users.noreply.github.com>
2025-04-14 20:02:17 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "",
   build: {
     outDir: "build",
     assetsDir: "assets",
@@ -16,12 +16,4 @@ export default defineConfig({
       },
     },
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  }
 });

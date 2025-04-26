d5186aea67b89a5fcaf85accea863088b9f4fa20
YoussefWaleed2 <114354330+YoussefWaleed2@users.noreply.github.com>
2025-04-26 10:42:25 +0300

    Update emailConfig.js

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -1,6 +1,6 @@
 // Brevo Email Configuration
 export const emailConfig = {
-  apiKey: "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", // Your Brevo API key
+  apiKey: "#", // Your Brevo API key
   senderEmail: "youssef.waleed2231@gmail.com", // Sender email for Brevo
   senderName: "VZBL", // Sender name
   recipientEmails: [
